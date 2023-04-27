efd8d64e8f7557d00dfb2eee7e72980f9e91c43a
UlyssePerret <ulysseperret@orange.fr>
2023-04-27 11:48:01 +0200

    Add files via upload
    Mastermind add

---
+++ mastermind/script.js
@@ -0,0 +1,21 @@
+
+/**Scrip**/
+
+/**Mode d'emploi**/
+var btnPopup = document.getElementById("btnPopup");
+var overlay = document.getElementById("overlay");
+var btnClose = document.getElementById("btnClose");
+ 
+
+btnPopup.addEventListener('click',openModal);
+
+function openModal(){
+	overlay.style.display = 'block';
+}
+
+btnClose.addEventListener('click',closePopup);
+
+function closePopup(){
+	overlay.style.display = 'none';
+}
+ 
